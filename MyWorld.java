9526e82c98c71fa83d98e0cb28a592da89b5a32c
Uriel Ngadeu <ungadeu27@gmail.com>
2024-04-04 12:38:57 -0400

    Remade Start Screen, fits new dimension

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,13 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1100, 800, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
