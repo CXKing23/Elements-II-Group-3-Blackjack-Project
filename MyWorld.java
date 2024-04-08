88c1410eaea9123378ed8762efe6e5312e5e63fa
MustafaProgram <156232890+MustafaProgram@users.noreply.github.com>
2024-04-08 13:41:00 -0400

    Revert "Revert "Revert "I made a button class"""

--- MyWorld.java
+++ MyWorld.java
@@ -17,10 +17,6 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1100, 800, 1); 
-
-        
-        addObject(new settingsButton(), getWidth() / 2 , getHeight() / 2 + 250);
-
         prepare();
     }
     /**
@@ -29,7 +25,5 @@ public class MyWorld extends World
      */
     private void prepare()
     {
-
     }
-    
 }
