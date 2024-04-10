973f77d8c9c1c20be3f3dcd7d5cea90da9ad0f5d
tylererv <tylerervin193176@yahoo.com>
2024-04-10 14:41:01 -0400

    Should enumerate Witch and FT and add pictures to the two.

--- Suit.java
+++ Suit.java
@@ -1,13 +1,13 @@
 /**
  * Write a description of class Suit here.
- * 
- * @author (your name) 
+ *
+ * @author (your name)
  * @version (a version number or a date)
  */
-public enum Suit  
+public enum Suit
 {
     CLUB(0,"\u2663"), SPADE(1, "\u2660"),
-    HEART(2, "\u2665"), DIAMOND(3, "\u2666");
+    HEART(2, "\u2665"), DIAMOND(3, "\u2666"), SPECIAL(4, "Special");
 
     private int value;
     private String rep;
@@ -16,13 +16,12 @@ public enum Suit
         this.value = value;
         rep = representation;
     }
-    
+
     public int getValue(){
-	return value;
+        return value;
     }
-    
+
     public String toString(){
         return rep;
     }
-
 }
