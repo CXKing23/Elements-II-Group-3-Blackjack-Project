875f5c5c8caed8727c87947b760992d9531a5e17
CXKing23 <156232701+CXKing23@users.noreply.github.com>
2024-04-18 11:39:41 -0400

    Update background3.java
    fixed image file path

--- background3.java
+++ background3.java
@@ -17,7 +17,7 @@ public class background3 extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(1100, 800, 1); 
-        setBackground("background3");
+        setBackground("background3.png");
         addObject(new homeButton(), getWidth() / 2 -500 , getHeight() / 2 );
     }
 }
