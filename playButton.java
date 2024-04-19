aa5351eaaeb5c1655ecbe767f6483eb450f01991
MustafaProgram <mustafaakilli24@gmail.com>
2024-04-19 19:54:09 -0400

    I made the buttons have depth by having the buttons have delay and switch between the different button.pngs also I made a hit and stand button in play state so when we code switching between the states the hit and stand button will appear.

--- playButton.java
+++ playButton.java
@@ -12,7 +12,7 @@ public class playButton extends button
      * Act - do whatever the playButton wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    GreenfootImage image = new GreenfootImage("redButton.png");
+    GreenfootImage image = new GreenfootImage("activeButton.png");
     public playButton(){
         image.scale(170,80);
         setImage(image);
@@ -21,15 +21,31 @@ public class playButton extends button
         image.drawString("Play", 60, 48);
     }
     public void onClick(){
-
+    image= new GreenfootImage("redButton.png");
+        image.scale(170,80);
+        setImage(image);
+        image.setColor(Color.BLACK); 
+        image.setFont(new Font("Arial", true, false, 24));
+        image.drawString("Play", 60, 48);
+        Greenfoot.delay(75);
+        GreenfootImage image = new GreenfootImage("activeButton.png");
+        image.scale(170,80);
+        setImage(image);
+        image.setColor(Color.BLACK); 
+        image.setFont(new Font("Arial", true, false, 24));
+        image.drawString("Play", 60, 48);
         if(count == 0){
             Greenfoot.setWorld(new background("background1.png"));
+            
         }else if(count == 1){
             Greenfoot.setWorld(new background("background2.png"));
+            
         }else if(count == 2){
             Greenfoot.setWorld(new background("background3.png"));
+            
         }else if(count == 3){
             Greenfoot.setWorld(new background("background4.png"));
+            
     }
 }
 }
