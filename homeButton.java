7a136d9f5f9d054b727926378929b18b0c40362b
CXKing23 <156232701+CXKing23@users.noreply.github.com>
2024-04-28 14:00:30 -0400

    Document homeButton.java
    Documentation Added

--- homeButton.java
+++ homeButton.java
@@ -1,26 +1,45 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class homeButton here.
+ * Represents a button that navigates to the start screen when clicked.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Inherits from the Actor class and overrides the act method to define the behavior when clicked.
+ * 
+ * When clicked, the button changes the world to the StartScreen if it's in the game world.
+ * If not in the game world, it displays a warning screen before navigating to the StartScreen.
+ * 
+ * The button is typically represented by an image of a home icon.
+ * 
+ * @author (Your Name)
+ * @version (Version Number or Date)
  */
 public class homeButton extends Actor
 {
+    private boolean inStartScreen = true; // Indicates if currently in the start screen
+    
     /**
-     * Act - do whatever the homeButton wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor for creating a homeButton.
+     * Initializes the button with default settings.
      */
-    public boolean inStartScreen = true;
     public homeButton(){
-        
+        // Additional initialization can be added here if needed
     }
+    
+    /**
+     * Method called when the homeButton is clicked.
+     * Changes the world to the StartScreen if in the game world.
+     * Displays a warning screen and then navigates to the StartScreen if not in the game world.
+     */
     public void act() {
-        setImage("castle.png");
+        setImage("castle.png"); // Set the image of the button to a home icon
+        
+        // Check if the button is clicked
         if (Greenfoot.mouseClicked(this)) {
-            Greenfoot.setWorld(new StartScreen()); 
-            if (!inStartScreen)  {
+            // If currently in the game world, navigate to the StartScreen directly
+            if (inStartScreen)  {
+                Greenfoot.setWorld(new StartScreen()); 
+            } else {
+                // If not in the game world, display a warning screen before navigating to the StartScreen
                 GameWorld currentGameWorld = (GameWorld)getWorld();
                 Greenfoot.setWorld(new warningScreen(currentGameWorld));
             }
@@ -28,5 +47,3 @@ public class homeButton extends Actor
     }
 
 }
-
-
