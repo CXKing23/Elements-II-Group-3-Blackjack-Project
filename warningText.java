d3b817dcc94b3b185aa77921272c3e6513c4e476
CXKing23 <156232701+CXKing23@users.noreply.github.com>
2024-04-28 14:10:21 -0400

    Document warningText.java
    Documentation Added And Change Warning to  "Are you sure you want to exit? Any progress made will be lost."

--- warningText.java
+++ warningText.java
@@ -1,24 +1,29 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class warningText here.
+ * Represents a text display for a warning message.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * This class extends the Actor class and displays a warning message.
+ * It scales and sets the image of the warning text.
+ * 
+ * @author (Your Name)
+ * @version (Version Number or Date)
  */
 public class warningText extends Actor
 {
     /**
-     * Act - do whatever the warningText wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Defines the action to be performed by the warningText.
+     * This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
-    
     public void act()
     {
-        GreenfootImage image = new GreenfootImage("Are you sure you want to exit, your progress will not be saved.", 100, Color.BLACK, new Color(0, 0, 0, 0));
+        // Create a GreenfootImage for the warning text
+        GreenfootImage image = new GreenfootImage( "Are you sure you want to exit? Any progress made will be lost.", 100, Color.BLACK, new Color(0, 0, 0, 0));
 
-        image.scale(1000,100);
-        setImage(image);
+        // Scale the image to fit the screen
+        image.scale(1000, 100);
         
+        // Set the image of the warning text
+        setImage(image);
     }
 }
