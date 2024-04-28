001e7a20e1d1ca6e6973d2ed80e0bb1d3f2f44cb
CXKing23 <156232701+CXKing23@users.noreply.github.com>
2024-04-28 13:51:41 -0400

    Document GameState.java
    Documentation Added

--- GameState.java
+++ GameState.java
@@ -5,5 +5,12 @@
  * @version (a version number or a date)
  */
 public enum GameState {
-    START_SCREEN, BET, DEAL, PLAY, D_PLAY, FIND_WINNER, GAME_OVER, RESET;
+    START_SCREEN, // State representing the start screen
+    BET,          // State representing the betting phase
+    DEAL,         // State representing the dealing of cards
+    PLAY,         // State representing the player's turn
+    D_PLAY,       // State representing the dealer's turn
+    FIND_WINNER,  // State representing the process of finding the winner
+    GAME_OVER,    // State representing the end of the game
+    RESET;        // State representing the game reset
 }
