a462f93a66936667918df8ad188b94f65fa05d93
CXKing23 <156232701+CXKing23@users.noreply.github.com>
2024-04-28 13:50:35 -0400

    Document Currency.java
    Added Documentation

--- Currency.java
+++ Currency.java
@@ -1,37 +1,58 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Currency here.
+ * Class representing the Currency display in the game.
+ * This class manages the current balance and bet balance, displaying them on the screen.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * It includes methods for setting and getting the balance, as well as updating the display image.
+ * 
+ * It retrieves the balance from the `bronzeChip` class, assuming it has appropriate methods for accessing the balance and bet balance.
+ * 
+ * @author (Your Name)
+ * @version (Version Number or Date)
  */
 public class Currency extends Actor
 {
-    /**
-     * Act - do whatever the Currency wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     static int currentBalance;
     static int betBalance; 
     static final int startingBalance = 1000;
     GreenfootImage image; 
+    
+    /**
+     * Constructor for creating a Currency object.
+     * Initializes the current balance and bet balance from the `bronzeChip` class and sets the display image.
+     */
     public Currency()
     {
         currentBalance = bronzeChip.getBalance();
         betBalance = bronzeChip.getbetBalance();
         setImage(image);
     }
+    
+    /**
+     * Sets the current balance to the specified value.
+     * 
+     * @param newBalance The new balance to set.
+     */
     public void setBalance(int newBalance){
         currentBalance = newBalance;
     }
    
+    /**
+     * Gets the current balance.
+     * 
+     * @return The current balance.
+     */
     public int getBalance() {
         return currentBalance;
     }
     
-     public void act(){
-            image = new GreenfootImage("Balance: " + bronzeChip.getBalance() + "\n" + "Bet: " + bronzeChip.getbetBalance(), 24, Color.BLACK, new Color(0, 0, 0, 0));
-            setImage(image);
-            }
+    /**
+     * Act method to update the display image with the current balance and bet balance.
+     */
+    public void act(){
+        // Update the image with the current balance and bet balance
+        image = new GreenfootImage("Balance: " + bronzeChip.getBalance() + "\n" + "Bet: " + bronzeChip.getbetBalance(), 24, Color.BLACK, new Color(0, 0, 0, 0));
+        setImage(image);
+    }
 }
