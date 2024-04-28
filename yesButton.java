0de0ea495ce22e409b6b1f60af24b604e1b3b27b
CXKing23 <156232701+CXKing23@users.noreply.github.com>
2024-04-28 14:11:22 -0400

    Document yesButton.java
    Documentation Added

--- yesButton.java
+++ yesButton.java
@@ -1,43 +1,55 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
 /**
- * Write a description of class yesButton here.
+ * Represents a button to confirm a "Yes" action for WarningScreen.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * This class extends the button class and overrides the onClick method to define the behavior when clicked.
+ * 
+ * @author (Your Name)
+ * @version (Version Number or Date)
  */
 public class yesButton extends button
 {
-    
     /**
-     * Act - do whatever the yesButton wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Constructor for creating a yesButton.
+     * Initializes the button with the default appearance and text.
      */
     public yesButton()
     {
+        // Create the button image with default appearance and text
         image = new GreenfootImage("activeButton.png");
-            image.scale(170,80);
-            setImage(image);
-            image.setColor(Color.BLACK);
-            image.setFont(new Font("Arial", true, false, 24));
-            image.drawString("Yes", 60, 48);
-        }
+        image.scale(170, 80);
+        setImage(image);
+        image.setColor(Color.BLACK);
+        image.setFont(new Font("Arial", true, false, 24));
+        image.drawString("Yes", 60, 48);
+    }
+    
+    /**
+     * Method called when the yesButton is clicked.
+     * Changes the appearance of the button when clicked and triggers an action to return to the StartScreen.
+     */
     public void onClick() {
+        // Change button appearance
         image = new GreenfootImage("redButton.png");
-        image.scale(170,80);
+        image.scale(170, 80);
         setImage(image);
         image.setColor(Color.BLACK);
         image.setFont(new Font("Arial", true, false, 24));
         image.drawString("Yes", 60, 48);
+        
+        // Delay to show button click effect
         Greenfoot.delay(duration);
+        
+        // Reset button appearance
         GreenfootImage image = new GreenfootImage("activeButton.png");
-        image.scale(170,80);
+        image.scale(170, 80);
         setImage(image);
         image.setColor(Color.BLACK);
         image.setFont(new Font("Arial", true, false, 24));
         image.drawString("Yes", 60, 48);
+        
+        // Switch to the StartScreen world
         Greenfoot.setWorld(new StartScreen());
     }
 }
-
-
