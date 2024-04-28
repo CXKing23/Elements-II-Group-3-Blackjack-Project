858332cd1bb676e7dafc0a30acbea26c8af589dd
CXKing23 <156232701+CXKing23@users.noreply.github.com>
2024-04-28 14:04:47 -0400

    Document settingsButton.java
    Documentation Added

--- settingsButton.java
+++ settingsButton.java
@@ -1,46 +1,55 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class settingsButton here.
+ * Represents a button for accessing the settings screen in the game.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * This class extends the button class and defines the behavior of the settingsButton.
+ * When clicked, it switches the world to the setting screen by creating a new instance of settingScreen.
+ * 
+ * @author (Your Name)
+ * @version (Version Number or Date)
  */
 public class settingsButton extends button
 {
-    
-        
-    
-    /**
-     * Act - do whatever the settingsButton wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    
     GreenfootImage image = new GreenfootImage("activeButton.png");
 
+    /**
+     * Constructor for creating a settingsButton.
+     * Initializes the button with the default appearance and text.
+     */
     public settingsButton(){
-        image.scale(170,80);
+        image.scale(170, 80);
         setImage(image);
         image.setColor(Color.BLACK); 
         image.setFont(new Font("Arial", true, false, 24));
         image.drawString("Setting", 45, 48);
     }
+    
+    /**
+     * Defines the behavior of the settingsButton when clicked.
+     * Switches the world to the setting screen.
+     */
     public void onClick() {
-        // Switch the world to SettingScreen
-        image= new GreenfootImage("redButton.png");
-        image.scale(170,80);
+        // Change the appearance of the button when clicked
+        image = new GreenfootImage("redButton.png");
+        image.scale(170, 80);
         setImage(image);
         image.setColor(Color.BLACK); 
         image.setFont(new Font("Arial", true, false, 24));
         image.drawString("Setting", 45, 48);
+        
+        // Add a delay to show the clicked state
         Greenfoot.delay(duration);
-        GreenfootImage image = new GreenfootImage("activeButton.png");
-        image.scale(170,80);
+        
+        // Restore the original appearance of the button
+        image = new GreenfootImage("activeButton.png");
+        image.scale(170, 80);
         setImage(image);
         image.setColor(Color.BLACK); 
         image.setFont(new Font("Arial", true, false, 24));
         image.drawString("Setting", 45, 48);
+        
+        // Switch the world to the setting screen
         Greenfoot.setWorld(new settingScreen());
     }
-
-    }
+}
